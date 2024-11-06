7b12cf7045daae026740cfce3525c43b15471d0d
D_Martins21 <davi_martins2009@outlook.com>
2024-11-06 15:56:26 -0300

    Aula 13 exercicio

--- Aulas Basicas/aula_de_console.js
+++ Aulas Basicas/aula_de_console.js
@@ -3,3 +3,12 @@ console.log('Luiz "Otavio"');
 console.log(`'Luiz' "Otavio"`);
 
 console.log(35,16.50,'Luiz Otavio de novo');
+
+let umaString = "o rato roeu a roupa do rei de roma";
+
+console.log(umaString.length);
+
+console.log(umaString.slice(3, 7));
+
+console.log(umaString.substring(0, 6));
+
